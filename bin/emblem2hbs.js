669fa6a4718d0bce28930c0e968d01aee0afb19d
Xavier Cambar <xcambar@gmail.com>
2015-10-31 10:45:12 +0100

    Generated files have .hbs extension

--- bin/emblem2hbs.js
+++ bin/emblem2hbs.js
@@ -11,7 +11,7 @@ if (process.argv.length < 3) {
   console.log('USAGE: emblem2hbs filetoconvert.emblem')
 } else {
   emblemFile = process.argv[2];
-  hbsFile = emblemFile.substr(0, emblemFile.lastIndexOf('.')) + '.js.hbs';
+  hbsFile = emblemFile.substr(0, emblemFile.lastIndexOf('.')) + '.hbs';
   buf = fs.readFileSync(emblemFile, 'utf8');
   output = processor.process(buf);
   fs.writeFileSync(hbsFile, indentation.indent(output));
