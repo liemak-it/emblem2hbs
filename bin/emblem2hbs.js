65048ea8efaab4a556347dfa47f5d449c578dace
Max Schwenk <maschwenk@gmail.com>
2016-09-06 01:31:02 -0400

    Allow for 2nd argument to be destination filename

--- bin/emblem2hbs.js
+++ bin/emblem2hbs.js
@@ -9,14 +9,13 @@ var fs = require('fs'),
 if (process.argv.length < 3) {
   if (process.stdin.isTTY) {
     console.log('USAGE: emblem2hbs filetoconvert.emblem or in piped format `pbcopy | emblem2hbs | pbpaste`');
-    return;
   }
   else {
     processFromPipe();
   }
 } else {
   emblemFile = process.argv[2];
-  hbsFile = emblemFile.substr(0, emblemFile.lastIndexOf('.')) + '.hbs';
+  hbsFile = process.argv[3] ? process.argv[3] : emblemFile.substr(0, emblemFile.lastIndexOf('.')) + '.hbs';
   buf = fs.readFileSync(emblemFile, 'utf8');
   output = require('emblem').default.compile(buf);
   fs.writeFileSync(hbsFile, indentation.indent(output));
